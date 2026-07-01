89700b0f4032fea5c2c92ec5f53f9bc95b1d5edc
TechQuery <shiy2008@gmail.com>
2026-07-02 04:14:14 +0800

    [fix] Package Test details

--- test/package.spec.ts
+++ test/package.spec.ts
@@ -1,9 +1,12 @@
-const packageJSON = require('../package.json');
+import packageJSON from '../package.json';
 
 describe('Package exports', () => {
-    it('should expose standard npm ESM and CommonJS entry points', () => {
+    it('should expose standard npm ESM & CJS entry points', () => {
+        expect(packageJSON.types).toBe('dist/index.d.ts');
+        expect(packageJSON.source).toBe('source/index.ts');
         expect(packageJSON.main).toBe('dist/index.js');
         expect(packageJSON.module).toBe('dist/index.mjs');
+
         expect(packageJSON.exports).toEqual({
             '.': {
                 types: './dist/index.d.ts',
